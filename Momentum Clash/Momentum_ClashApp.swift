2049a2811f8fcf96fd1c382d3e4d419930435d4c
JunLee8108 <lejhn1@gmail.com>
2026-03-19 15:12:36 -0500

    Initial Commit

---
+++ Momentum Clash/Momentum_ClashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Momentum_ClashApp.swift
+//  Momentum Clash
+//
+//  Created by Jun Lee on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Momentum_ClashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Momentum ClashTests/Momentum_ClashTests.swift
@@ -0,0 +1,17 @@
+//
+//  Momentum_ClashTests.swift
+//  Momentum ClashTests
+//
+//  Created by Jun Lee on 3/19/26.
+//
+
+import Testing
+@testable import Momentum_Clash
+
+struct Momentum_ClashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
